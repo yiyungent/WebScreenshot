f9f286b26a9a4a6bc7e0e615fab6e611862c2faf
yiyun <yiyungent@gmail.com>
2022-03-04 12:23:09 +0800

    feat(program.cs): 任何环境下 都启用 Swagger

--- src/WebScreenshot/Program.cs
+++ src/WebScreenshot/Program.cs
@@ -14,9 +14,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
